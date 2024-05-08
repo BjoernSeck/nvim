b8e9bdeef114860d8f4f01492b496ab36379d154
Björn Seck <bseck@eos-uptrade.de>
2024-05-08 13:33:24 +0200

    more remapps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,5 +7,9 @@ local map = vim.keymap.set
 map('t', '<ESC>', '<C-\\><C-n>', {noremap = true})
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>p", "\"_dP")
+map("n", "<leader>y", "\"+y")
+map("v", "<leader>y", "\"+y")
+map("n", "<leader>Y", "\"+Y")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
